dd3445fb3f8a138d4be1e692f91f615a58b7a285
Humber Aquino <humberaquino@gmail.com>
2015-07-22 19:13:37 -0400

    PORT added to Procfile

--- src/main/java/py/uca/infraestructura/Application.java
+++ src/main/java/py/uca/infraestructura/Application.java
@@ -10,15 +10,12 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+      String webPort = System.getenv("PORT");
+     if (webPort == null || webPort.isEmpty()) {
+         webPort = "8080";
+     }
+     System.setProperty("server.port", webPort);
+     SpringApplication.run(SampleController.class, args);
     }
 
 }
