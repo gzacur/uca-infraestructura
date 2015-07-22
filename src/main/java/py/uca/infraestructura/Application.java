722371dcf5f6045e0ae438947e36ffbe248ceed8
Humber Aquino <humberaquino@gmail.com>
2015-07-22 19:15:08 -0400

    Fix compilation error

--- src/main/java/py/uca/infraestructura/Application.java
+++ src/main/java/py/uca/infraestructura/Application.java
@@ -9,13 +9,13 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
       String webPort = System.getenv("PORT");
-     if (webPort == null || webPort.isEmpty()) {
-         webPort = "8080";
-     }
-     System.setProperty("server.port", webPort);
-     SpringApplication.run(SampleController.class, args);
+      if (webPort == null || webPort.isEmpty()) {
+          webPort = "8080";
+      }
+      System.setProperty("server.port", webPort);
+      SpringApplication.run(SampleController.class, args);
     }
 
 }
